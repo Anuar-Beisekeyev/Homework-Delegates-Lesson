2600bcf70485522c95a60b39a71b6009ef760cbd
Anuar <beisekeyev.a.1992.ast@gmail.com>
2021-02-16 11:44:11 +0600

    Added homework for the lesson Delegates

---
+++ HomeworkDelegatesLesson/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeworkDelegatesLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Ракета прошла все подготовительные работы.");
+            Console.WriteLine("Все параметры в норме");
+            Console.WriteLine("Нажмите кнопку <Пуск>");
+            Console.WriteLine();
+
+            RedButton redButton = new RedButton
+            {
+                SymbolicBorderOfTheButton = '*',
+                Text = "Пуск",
+                Width = 12,
+                Height = 6,
+                Action = new ActivateTheButtonDelegate<string>(Console.WriteLine)
+            };
+
+            redButton.PrintRedButton();
+            Console.WriteLine("Пуск успешно произведен !!!");
+            Console.ReadLine();
+            
+        }
+       
+    }
+}
---
+++ HomeworkDelegatesLesson/RedButton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkDelegatesLesson
+{
+    public delegate void ActivateTheButtonDelegate<T>(params T[] data);
+    public class RedButton
+    {
+        public char SymbolicBorderOfTheButton { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public string Text { get; set; }       
+        public ActivateTheButtonDelegate<string> Action { get; set; }
+       
+        public void PrintRedButton()
+        {
+            for(int i = 0; i < Height; i++)
+            {
+                for(int j = 0; j < Width; j++)
+                {
+                    if(j == 0 | i == 0 | j == Width - 1 | i == Height - 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.DarkGray; 
+                        Console.Write(SymbolicBorderOfTheButton);
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.SetCursorPosition(4,6);
+            Console.Write(Text);
+            Console.ResetColor();
+            Console.ReadLine();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+        }        
+    }
+}
